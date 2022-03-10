f79e12b112121fdae2466e3d1e9bfd2d00d69ece
welkenburg <48929649+welkenburg@users.noreply.github.com>
2022-03-10 17:09:21 +0000

    gzorg

---
+++ grid.ts
@@ -0,0 +1,13 @@
+// const fs = require("fs/promises");
+import { promises as fsp } from "fs";
+
+const input = [
+  [1],
+  [5,6]
+]
+
+const main = async () => {
+  console.log("test");
+}
+
+main();
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-inputFile = input()
+inputFile = "i.txt"
 
 def computeNumberDays(NbPlayers, MaxDays, PlayerIncrements):
-    pass
-    return NbDays
+  # Do stuff here
+  counter = 0
+  for i in range(MaxDays):
+    counter += 1
+    for j in PlayerIncrements:
+      if i % j != 0:
+        counter -= 1
+        break
+    
+  return counter
 
 output = []
 
 with open(inputFile, "r") as cases:
     lines = cases.readlines()
     T = int(lines[0])
-    for i in range((len(lines)-1)/2):
+    for i in range((len(lines)-1)//2):
         N, M    = map(int,lines[1 + i*2 + 0].split(" "))
-        P       = map(int, lines[1+ i*2 + 1].split(" "))
-        output.append(computeNumberDays(N,M,P))
-
-T = 0
-S = 0
-
-for i in T:
-    print(f"Case #{i}: {S}")
+        P  = list(map(int, lines[1+ i*2 + 1].split(" ")))
+        output.append(f"Case #{i}: {computeNumberDays(N,M,P)}")
+    for i in output:
+      print(i)
